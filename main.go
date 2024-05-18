b9876053b0516f00090de06f090e7da00a463201
sakhdevel <sakhdevel@yandex.ru>
2024-05-18 16:39:09 +0400

    Registering a POST route

--- main.go
+++ main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"sakhdevel/go-web-service/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8085") // localhost
 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	// todo change later
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
