9178e7acfafff4aa2831db27dc7423239e74d1de
sakhdevel <sakhdevel@yandex.ru>
2024-05-21 21:03:02 +0400

    Storing data in db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"sakhdevel/go-web-service/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,10 +16,26 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// later: add it to database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
 }
+
 func GetAllEvents() []Event {
 	return events
 }
