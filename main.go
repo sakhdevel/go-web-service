44612b3654f11afec1fb7755e4fcd49d8153f921
sakhdevel <sakhdevel@yandex.ru>
2024-05-25 11:55:55 +0400

    Getting Single Event Data By ID

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"sakhdevel/go-web-service/db"
 	"sakhdevel/go-web-service/models"
+	"strconv"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8085") // localhost
@@ -28,6 +30,28 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"message": "Could not parse event Id"},
+		)
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "Could not fetch event"},
+		)
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
