027433821378e87056b6556452c92f514efe9300
sakhdevel <sakhdevel@yandex.ru>
2024-05-25 21:34:47 +0400

    Generating JWT

---
+++ utils/jwt.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const secretKey = "supersecret"
+
+func GenerateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"email":  email,
+			"userId": userId,
+			"exp":    time.Now().Add(time.Hour * 2).Unix(),
+		})
+
+	return token.SignedString(secretKey)
+}
