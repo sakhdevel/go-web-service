3043896259a5138974a540ec6038a86199d11300
sakhdevel <sakhdevel@yandex.ru>
2024-05-26 16:53:01 +0400

    Token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
@@ -17,4 +18,42 @@ func GenerateToken(email string, userId int64) (string, error) {
 		})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("invalid token")
+	}
+
+	// use type checking syntax. it tries to convert to what
+	// is inside parenthesis and if ok, it will store
+	// converted version in "claims" and bool result of converting
+	// in "ok".
+	//claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	//
+	//if !ok {
+	//	return errors.New("invalid token claims")
+	//}
+	//
+	//email := claims["email"].(string)
+	//userId := claims["userId"].(int64)
+
+	// todo
+	return nil
 }
