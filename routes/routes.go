540b2306d5d4dce09ab9c347c8374de2cc9ef8f3
sakhdevel <sakhdevel@yandex.ru>
2024-06-01 15:29:01 +0400

    Testing & Fixing the "Register" Route

--- routes/routes.go
+++ routes/routes.go
@@ -16,7 +16,7 @@ func RegisterRoutes(server *gin.Engine) {
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
 
-	authenticated.POST("/events/:id/register")
+	authenticated.POST("/events/:id/register", registerForEvent)
 	authenticated.DELETE("/events/:id/register")
 
 	server.POST("/signup", signup)
